d81e1dac5348a14958f2bb30b31189c49bfa5b57
Maritza Loaiza Urrutia <mloaiza.urrutia@gmail.com>
2021-06-05 22:22:58 -0400

    se cambian operadores por radio buttons

--- js/math-operations.js
+++ js/math-operations.js
@@ -7,6 +7,21 @@ let restar;
 let dividir;
 let multiplicar;
 
+$(document).ready(function(){
+    $("input[name=operators]").click(function () {   
+        console.log("seleccion: " + $(this).val()) ;
+        select = $(this).val();
+        $("label[name=labelOperator]").css("background-color", "#D5D6E9");
+        $("div[name=operator]").css("color", "#111C81");
+        $("#"+select).css("background-color", "#5563E6");
+        $("#label"+select).css("color", "#FFFFFF");
+        temp = input.value;
+        labelTemp.innerText = temp;
+        console.log(temp);
+        input.value = "";
+    });
+});
+
 function pressNumber(number) {
     if(input.value === "0"){
         console.log("if");
@@ -18,59 +33,21 @@ function pressNumber(number) {
 function clearInput() {
     input.value = 0;
     labelTemp.innerText = "";
-}
-
-function suma() {
-    sumar = true;
-    restar = false;
-    multiplicar = false;
-    dividir = false;
-    temp = input.value;
-    labelTemp.innerText = temp;
-    input.value = "";
-}
-
-function resta() {
-    sumar = false;
-    restar = true;
-    multiplicar = false;
-    dividir = false;
-    temp = input.value;
-    labelTemp.innerText = temp;
-    input.value = "";
-}
-
-function multiplicacion() {
-    sumar = false;
-    restar = false;
-    multiplicar = true;
-    dividir = false;
-    temp = input.value;
-    labelTemp.innerText = temp;
-    input.value = "";
-}
-
-function division() {
-    sumar = false;
-    restar = false;
-    multiplicar = false;
-    dividir = true;
-    temp = input.value;
-    labelTemp.innerText = temp;
-    input.value = "";
+    $("label[name=labelOperator]").css("background-color", "#D5D6E9");
+    $("div[name=operator]").css("color", "#111C81");
 }
 
 function resultado() {
-    if(sumar){
+    if(select === "suma"){
         console.log("sumando");
         input.value = parseInt(temp) + parseInt(input.value);
-    } else if(restar){
+    } else if(select === "resta"){
         console.log("restando");
         input.value = parseInt(temp) - parseInt(input.value);
-    } else if(multiplicar){
+    } else if(select === "multiplicacion"){
         console.log("multiplicando");
         input.value = parseInt(temp) * parseInt(input.value);
-    } else if(dividir){
+    } else if(select === "division"){
         console.log("dividiendo");
         input.value = parseInt(temp) / parseInt(input.value);
     }
